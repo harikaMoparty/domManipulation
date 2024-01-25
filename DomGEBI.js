d8887b3b2f9086c91633a97d4e6717111d525963
harikaMoparty <157496159+harikaMoparty@users.noreply.github.com>
2024-01-25 16:18:30 +0530

    First Dom getElementById

---
+++ DomGEBI.js
@@ -0,0 +1,13 @@
+const header = document.getElementById("main-heading");
+header.textContent = "Fruit World";
+header.style.color = "orange";
+
+const heading = document.getElementById("header");
+heading.style.backgroundColor = "green";
+heading.style.borderBottom = "orange";
+
+const Basket = document.getElementById("basket-heading");
+Basket.style.color = "green";
+
+const Greeting = document.getElementById("thanks");
+Greeting.innerHTML = "<p>Please visit us again</p>";
